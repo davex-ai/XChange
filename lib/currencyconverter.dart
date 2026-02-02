9a7ce034acfb5637a1e149f42b190fff526127c4
davex-ai <omotoyedave4@gmail.com>
2026-02-02 21:51:02 +0100

    ....

--- lib/currencyconverter.dart
+++ lib/currencyconverter.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Currencyconverter extends StatelessWidget {
-  const Currencyconverter({super.key});
+class CurrencyConverter extends StatelessWidget {
+  const CurrencyConverter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('0', style: TextStyle(fontSize: 55, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255)), )
-            TextField()
+            const Text(
+              '0',
+              style: TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Enter amount in USD',
+                hintStyle: TextStyle(color: Colors.white60),
+                prefix: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.white60,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.circular(60),
+                ),
+              ),
+            ),
           ],
         ),
       ),
